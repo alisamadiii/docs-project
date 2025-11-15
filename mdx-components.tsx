86386da7854a1a569397e46f4c632afaaf1c51b3
Ali Samadi <a@alisamadii.com>
2025-11-15 07:04:43 -0800

    feat: added cloudfront docs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,21 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import type { MDXComponents } from "mdx/types";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Banner } from "fumadocs-ui/components/banner";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => (
+      <ImageZoom
+        className="outline-2 outline-black/10 rounded-xl outline-offset-[-2px]"
+        {...(props as any)}
+      />
+    ),
+    Step,
+    Steps,
+    Banner,
     ...components,
   };
 }
